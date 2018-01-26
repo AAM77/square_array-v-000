9a41ae861a71bcb88b2da4402e4da3999cc3839b
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-01-26 19:30:10 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+numbers = [1, 2, 3 ,4 ,5 ,6 ,7, 8, 9, 10]
+
 def square_array(array)
-  # your code here
+  array.each do |number|
+    squared = number * number
+    new_array.new.push(squared)
+  end
+  return new_array
 end
+
+square_array(numbers)
